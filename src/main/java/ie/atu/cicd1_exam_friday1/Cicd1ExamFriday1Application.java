f24d5d770f8894e75227894dd228870ec277f135
DAVIDPRENDERGAST-STU <g00405007@atu.ie>
2025-11-21 09:15:58 +0000

    First Commit

---
+++ src/main/java/ie/atu/cicd1_exam_friday1/Cicd1ExamFriday1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1_exam_friday1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd1ExamFriday1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd1ExamFriday1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/cicd1_exam_friday1/Cicd1ExamFriday1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1_exam_friday1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1ExamFriday1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
